0a004dc10b5b0b9b7b026efae6d61b14251be542
Rakibul73 <rakibul29185@yahoo.com>
2024-08-28 22:20:37 +0600

    added db conncetion in signup route

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,6 +1,31 @@
 from pydantic import BaseModel, EmailStr, constr
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+import pytz
+
+Base = declarative_base()
+
+def current_time():
+    return datetime.datetime.now(tz=pytz.timezone('UTC'))
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(30), unique=True)
+    email = Column(String(100), index=True)
+    first_name = Column(String(50), nullable=True)
+    last_name = Column(String(50), nullable=True)
+    password_hash = Column(String(255))
+    mobile = Column(String(20), nullable=True)
+    created_at = Column(DateTime(timezone=True), default=current_time, nullable=False)
+    updated_at = Column(DateTime(timezone=True), default=current_time, nullable=False, onupdate=current_time)
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
+
 
 class UserCreate(BaseModel):
+    username: str
     first_name: str
     last_name: str
     email: EmailStr
@@ -10,6 +35,7 @@ class UserCreate(BaseModel):
     class Config:
         schema_extra = {
             "example": {
+                "username": "John",
                 "first_name": "John",
                 "last_name": "Doe",
                 "email": "johndoe@example.com",
@@ -20,4 +46,4 @@ class UserCreate(BaseModel):
 
     def validate_passwords(self):
         if self.password != self.confirm_password:
-            raise ValueError("Passwords do not match")
+            raise ValueError("Passwords & confirm passwords do not match")
