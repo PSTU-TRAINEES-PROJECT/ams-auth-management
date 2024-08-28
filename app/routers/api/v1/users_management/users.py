3a7314fd92efc7cca135daa681a5f7afa2151d7f
Rakibul73 <rakibul29185@yahoo.com>
2024-08-28 22:25:36 +0600

    added db conncetion in signup route

--- app/routers/api/v1/users_management/users.py
+++ app/routers/api/v1/users_management/users.py
@@ -1,22 +1,24 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
+from repository.database import get_db
 from schemas.users import UserCreate
 from repository.user_repository import UserRepository
 from services.users import UserService
+from sqlalchemy.ext.asyncio import  AsyncSession
 
 user_router = APIRouter()
 
-repository = UserRepository()  # Using mock repository for now
+repository = UserRepository()
 user_service = UserService(repository)
 
 @user_router.post("/signup")
-async def signup(user: UserCreate):
+async def signup(user: UserCreate, db: AsyncSession = Depends(get_db)):
     try:
-        new_user = user_service.create_user(user)
-        return {"msg": "User created successfully", "user": new_user}
+        new_user = await user_service.create_user(user, db)
+        return {"message": "User created successfully", "user": new_user}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-# # this temporary endpoint to retrieve all users
-# @user_router.get("/users")
-# async def get_users():
-#     return repository.mock_db  # Directly return the mock database contents
+@user_router.get("/all-users")
+async def get_users(db: AsyncSession = Depends(get_db)):
+    users = await user_service.get_all_users(db)
+    return users
--- app/services/users.py
+++ app/services/users.py
@@ -1,27 +1,31 @@
+from utils.helpers.converters import hash_password, verify_password
 from repository.user_repository import UserRepository
 from schemas.users import UserCreate
+from sqlalchemy.ext.asyncio import  AsyncSession
 
 class UserService:
     def __init__(self, repository: UserRepository):
         self.repository = repository
 
-    def create_user(self, user: UserCreate):
+    async def create_user(self, user: UserCreate, db: AsyncSession):
         user.validate_passwords()
         
         # Check if user already exists
-        existing_user = self.repository.get_user_by_email(user.email)
+        existing_user = await self.repository.get_user_by_email(user.email, db)
         if existing_user:
             raise ValueError("User with this email already exists")
 
-        # Simulate saving user to the database
+        hashed_password = hash_password(user.password)
+
         user_data = {
+            "username": user.username,
             "first_name": user.first_name,
             "last_name": user.last_name,
             "email": user.email,
-            "password": user.password  # In real life, hash the password before saving
+            "password": hashed_password,
         }
-        user_name = {
-            "first_name": user.first_name,
-            }
 
-        return self.repository.save_user(user_name)
+        return await self.repository.save_user(user_data, db)
+    
+    async def get_all_users(self, db: AsyncSession):
+        return await self.repository.get_all_users(db)
